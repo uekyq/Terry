6ac683c35aa3df602e7f5661cb9acd3e895c6cf2
uekyq <43017592+uekyq@users.noreply.github.com>
2021-06-12 14:57:16 -0700

    Create terrySort.py

---
+++ terrySort.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+
+# Creating a Label Widget
+myLabel = Label(root, text="Hello, World!")
+myLabel.pack()
+canvas = Canvas(root, width = 300, height = 300)
+canvas.pack()
+img = ImageTk.PhotoImage(file="C:/Users/Uekyq/Documents/Scripts/Python/tkinter/unicorn.jpg")
+img2 = ImageTk.PhotoImage(file="C:/Users/Uekyq/Documents/Scripts/Python/tkinter/unicorn2.jpg")
+canvas.create_image(20,20, anchor=NW, image=img)
+def swapImg():
+    canvas.create_image(20,20, anchor=NW, image=img2)
+    return 1
+b = Button ( master=root, command=swapImg, text="ee")
+b.pack()
+
+root.mainloop()
