8f741c17023a6ff15ba8892e6b9ad26823c25dc2
uekyq <43017592+uekyq@users.noreply.github.com>
2021-06-14 19:43:50 -0700

    Update terrySort.py

--- terrySort.py
+++ terrySort.py
@@ -1,23 +1,31 @@
 from tkinter import *
 from PIL import ImageTk, Image
-
 root = Tk()
-
 image1 = Image.open("dune.jpg")
 def swapImg():
     print(image1.size)
     return 1
-# This line resizes the image to one fourth its original size, but I think it'd be better to make a functio that detects if the image is larger than the current window size and tries to resize the image by one integer factor lower until it completely fits within the window. Look up how to do this at work tomorrow.
-image1 = image1.resize((int(image1.size[0] / 4),int(image1.size[1] / 4)),Image.ANTIALIAS)
+# This line resizes the image to one fourth its original size, but I think it'd be
+# better to make a function that detects if the image is larger than the current window
+# size and tries to resize the image by one integer factor lower until it completely fits
+# within the window. Look up how to do this at work tomorrow.
+def scaleImage(imageSize):
+    windowDimensions = (1600,900)
+    imageHeight = 800
+    imageWidth = 800
+    i = 1
+    while(imageWidth > windowDimensions[0] and imageHeight > windowDimensions[1]):
+        i = i + 0.1
+        imageWidth = imageWidth / i
+        imageHeight = imageHeight / i
+        print(imageWidth)
+        print(imageHeight)
+    imageDimensions = (imageWidth, imageHeight)
+    return imageDimensions
 test = ImageTk.PhotoImage(image1)
-
 label1 = Label(image=test)
 label1.image = test
-
-myLabel = Label(root, text="Hello, World!")
-b = Button ( master=root, command=swapImg, text="ee")
-myLabel.pack()
-b.pack()
+b = Button ( master=root, command=scaleImage(image1.size), text="ee")
 label1.pack()
-
+b.pack()
 root.mainloop()
