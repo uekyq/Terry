5e11ff3b0fdedbf677e6bf104e8903be39edcdba
uekyq <43017592+uekyq@users.noreply.github.com>
2021-06-14 19:45:34 -0700

    Update terrySort.py

--- terrySort.py
+++ terrySort.py
@@ -9,23 +9,23 @@ def swapImg():
 # better to make a function that detects if the image is larger than the current window
 # size and tries to resize the image by one integer factor lower until it completely fits
 # within the window. Look up how to do this at work tomorrow.
-def scaleImage(imageSize):
-    windowDimensions = (1600,900)
-    imageHeight = 800
-    imageWidth = 800
-    i = 1
-    while(imageWidth > windowDimensions[0] and imageHeight > windowDimensions[1]):
-        i = i + 0.1
-        imageWidth = imageWidth / i
-        imageHeight = imageHeight / i
-        print(imageWidth)
-        print(imageHeight)
-    imageDimensions = (imageWidth, imageHeight)
-    return imageDimensions
+# def scaleImage(imageSize):
+#     windowDimensions = (1600,900)
+#     imageHeight = 800
+#     imageWidth = 800
+#     i = 1
+#     while(imageWidth > windowDimensions[0] and imageHeight > windowDimensions[1]):
+#         i = i + 0.1
+#         imageWidth = imageWidth / i
+#         imageHeight = imageHeight / i
+#         print(imageWidth)
+#         print(imageHeight)
+#     imageDimensions = (imageWidth, imageHeight)
+#     return imageDimensions
 test = ImageTk.PhotoImage(image1)
 label1 = Label(image=test)
 label1.image = test
-b = Button ( master=root, command=scaleImage(image1.size), text="ee")
+b = Button ( master=root, command=swapImg, text="ee")
 label1.pack()
 b.pack()
 root.mainloop()
