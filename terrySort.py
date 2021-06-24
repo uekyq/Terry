b223e5f35204f8adfe99ffdd787ac928f5f031ff
uekyq <43017592+uekyq@users.noreply.github.com>
2021-06-23 22:16:57 -0700

    Finished script
    turned script into command file based. The array that holds all the names of the files in the folder is sorted like windows sorts files.

--- terrySort.py
+++ terrySort.py
@@ -1,31 +1,45 @@
+# Imports
+from os import listdir
+from os.path import isfile, join, isdir
+import os.path
 from tkinter import *
 from PIL import ImageTk, Image
-root = Tk()
-image1 = Image.open("dune.jpg")
-def swapImg():
-    print(image1.size)
-    return 1
-# This line resizes the image to one fourth its original size, but I think it'd be
-# better to make a function that detects if the image is larger than the current window
-# size and tries to resize the image by one integer factor lower until it completely fits
-# within the window. Look up how to do this at work tomorrow.
-# def scaleImage(imageSize):
-#     windowDimensions = (1600,900)
-#     imageHeight = 800
-#     imageWidth = 800
-#     i = 1
-#     while(imageWidth > windowDimensions[0] and imageHeight > windowDimensions[1]):
-#         i = i + 0.1
-#         imageWidth = imageWidth / i
-#         imageHeight = imageHeight / i
-#         print(imageWidth)
-#         print(imageHeight)
-#     imageDimensions = (imageWidth, imageHeight)
-#     return imageDimensions
-test = ImageTk.PhotoImage(image1)
-label1 = Label(image=test)
-label1.image = test
-b = Button ( master=root, command=swapImg, text="ee")
-label1.pack()
-b.pack()
-root.mainloop()
+import os
+from PIL import Image
+from natsort import natsorted
+
+# A function that returns an array of all the files in the specified folder
+def fileArrayBuilder(folderPath):
+    myFiles = [f for f in listdir(folderPath) if isfile(join(folderPath, f))]
+    myFiles = natsorted(myFiles)
+    print(myFiles)
+    return myFiles
+
+# A function that moves the specified file into the specified folder
+def moveFile(fileName, folderName):
+    dirName = "E:/.Trash-1000/files/Visuals/Pictures/"
+    print("Moving " + dirName + fileName + " to " + dirName + folderName + "/" + fileName)
+    os.rename(dirName + fileName, dirName + folderName + fileName)
+
+# A function that gets the specified folder path and calls other functions
+def startFunc():
+    # Get folder path
+    # Get file array
+    # Get folder array
+    # Call windowLooper with file and folder arrays
+    # specifiedDir = input("Enter full path with drive letter of the folder you want to sort: ")
+    specifiedDir = "E:/.Trash-1000/files/Visuals/Pictures"
+    files = fileArrayBuilder(specifiedDir)
+    fileLooper(files)
+    
+def fileLooper(files):
+    for i in files:
+        specifiedDir = "E:/.Trash-1000/files/Visuals/Pictures/"
+        print(i)
+        #im = Image.open(specifiedDir + i)
+        #im.show()
+        fol = input("Specify folder: ")
+        moveFile(i, fol)
+
+# Start the script
+startFunc()
